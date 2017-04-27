82fb41ab9f38298b53924c3915da2ea735e59427
Raul Martin <alu0100769579@ull.edu.es>
2017-04-27 02:34:55 +0000

    Realizando cambios en los test

--- test/test.js
+++ test/test.js
@@ -6,7 +6,13 @@ require("should");
 
 describe("getArea", function() {
   
-  it("must compute the triangle area correctly. Rectangle object", function() {
+  it("must compute the rectangle shape area correctly", function(){
+		let a = new Shape ({ width: 100, height: 50 }, 'Rectangle');
+		let s = a.getArea();
+		s.should.equal(5000);
+	});
+  
+  it("must compute the rectangle area correctly. Rectangle object", function() {
     let a = new Rectangle({ width: 100, height: 50 });
     let s = a.getArea();
     s.should.equal(5000);
